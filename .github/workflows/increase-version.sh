ca6227daf655bf5298e402c39a88b6394f1ea61a
Mark Manirko <->
2022-11-20 23:48:25 +0100

    test-publish

--- .github/workflows/increase-version.sh
+++ .github/workflows/increase-version.sh
@@ -3,9 +3,9 @@
 FULL_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo $FULL_VERSION
 
-MAJOR_VERSION=$(cut -d "." -f1 <<< "$sss")
-MINOR_VERSION=$(cut -d "." -f2 <<< "$sss")
-PATCH_VERSION=$(cut -d "." -f3 <<< "$sss")
+MAJOR_VERSION=$(cut -d "." -f1 <<< "$FULL_VERSION")
+MINOR_VERSION=$(cut -d "." -f2 <<< "$FULL_VERSION")
+PATCH_VERSION=$(cut -d "." -f3 <<< "$FULL_VERSION")
 
 echo $((++MAJOR_VERSION))
 echo $((++MINOR_VERSION))
