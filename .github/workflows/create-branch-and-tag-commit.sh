d0b7425c474644e00f8ed8f40404c1499507bc46
Mark Manirko <->
2022-11-22 23:42:22 +0100

    test-publish

--- .github/workflows/create-branch-and-tag-commit.sh
+++ .github/workflows/create-branch-and-tag-commit.sh
@@ -5,3 +5,4 @@ FULL_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 git checkout -b FULL_VERSION
 git commit -m "release v ${FULL_VERSION}"
 git tag "v${FULL_VERSION}" HEAD
+git push origin --all
