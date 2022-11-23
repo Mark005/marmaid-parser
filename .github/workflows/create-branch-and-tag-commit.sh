13e5696c5e8b0bbd2d40de62a7bed8989adc8179
Mark Manirko <->
2022-11-23 21:21:07 +0100

    test-publish

--- .github/workflows/create-branch-and-tag-commit.sh
+++ .github/workflows/create-branch-and-tag-commit.sh
@@ -7,4 +7,6 @@ git config --global user.name "CI Action"
 git checkout -b "${FULL_VERSION}"
 git commit -m "release v${FULL_VERSION}"
 git tag "v${FULL_VERSION}" HEAD
+
+git config --get remote.origin.url
 git push origin --all
